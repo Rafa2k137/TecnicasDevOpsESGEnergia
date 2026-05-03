6d31e02af1133d4253889d6505adfeef99709185
Rafa2k137 <contato.rafaelvlima@gmail.com>
2026-05-03 20:50:28 -0300

    Add files via upload

---
+++ app/src/test/java/br/com/energia/energia/runner/CucumberRunner.java
@@ -0,0 +1,8 @@
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = "cucumber.glue", value = "br.com.energia.energia.steps")
+public class CucumberRunner {
+}
