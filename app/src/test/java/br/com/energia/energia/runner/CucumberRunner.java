1c297f43e2322032622604c0bd55afd393cefba4
Rafa2k137 <contato.rafaelvlima@gmail.com>
2026-05-03 22:37:16 -0300

    Update CucumberRunner.java

--- app/src/test/java/br/com/energia/energia/runner/CucumberRunner.java
+++ app/src/test/java/br/com/energia/energia/runner/CucumberRunner.java
@@ -1,13 +1,16 @@
 package br.com.energia.energia.runner;
 
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.*;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.com.energia.energia.steps")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,summary")
+@ConfigurationParameter(key = "cucumber.plugin", value = "pretty,summary")
 public class CucumberRunner {
 }
