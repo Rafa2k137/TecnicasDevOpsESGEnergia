2ab8b5d424d065ed7301bd16e5974f50c23c3dfe
Rafa2k137 <contato.rafaelvlima@gmail.com>
2026-05-03 22:25:57 -0300

    Update EnergiaSteps.java

--- app/src/test/java/br/com/energia/energia/steps/EnergiaSteps.java
+++ app/src/test/java/br/com/energia/energia/steps/EnergiaSteps.java
@@ -13,6 +13,12 @@ public class EnergiaSteps {
     @Dado("que a API está disponível")
     public void apiDisponivel() {
         // opcional health check
+        given()
+            .baseUri("http://energia-api:8080")
+        .when()
+            .get("/health")
+        .then()
+            .statusCode(anyOf(is(200), is(404)));
     }
 
     @Quando("eu envio um GET para {string}")
@@ -24,8 +30,46 @@ public class EnergiaSteps {
                 .get(endpoint);
     }
 
+    @Quando("eu envio um POST para {string} com dados válidos")
+    public void postValido(String endpoint) {
+        response =
+            given()
+                .baseUri("http://energia-api:8080")
+                .contentType("application/json")
+                .body("{\"consumo\":100}")
+            .when()
+                .post(endpoint);
+    }
+
+    @Quando("eu envio um POST para {string} com dados inválidos")
+    public void postInvalido(String endpoint) {
+        response =
+            given()
+                .baseUri("http://energia-api:8080")
+                .contentType("application/json")
+                .body("{}")
+            .when()
+                .post(endpoint);
+    }
+
     @Então("o status da resposta deve ser {int}")
     public void validarStatus(int status) {
         response.then().statusCode(status);
+    }
+
+    @Então("o corpo da resposta deve conter o campo {string}")
+    public void validarCampo(String campo) {
+        response.then().body("$", hasKey(campo));
+    }
+
+    @Dado("que existe um consumo cadastrado")
+    public void consumoCadastrado() {
+        response =
+            given()
+                .baseUri("http://energia-api:8080")
+                .contentType("application/json")
+                .body("{\"consumo\":50}")
+            .when()
+                .post("/energia");
     }
 }
