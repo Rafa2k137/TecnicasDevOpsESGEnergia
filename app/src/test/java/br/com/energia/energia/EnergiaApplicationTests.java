26e53a23cc1194197da304f9570a16f1635fc39b
Rafa2k137 <contato.rafaelvlima@gmail.com>
2026-05-03 22:42:10 -0300

    Update EnergiaApplicationTests.java

--- app/src/test/java/br/com/energia/energia/EnergiaApplicationTests.java
+++ app/src/test/java/br/com/energia/energia/EnergiaApplicationTests.java
@@ -3,11 +3,9 @@ package br.com.energia.energia;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EnergiaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class EnergiaApplicationTests {
+	//@Test
+	//void contextLoads() {
+	//}
+//}
